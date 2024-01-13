e6e3cb8703f31881e6b8f14f9f9bdc14315f2f38
Dewa Nyoman Dharma Triyasa <dewa.triyasa@gopay.co.id>
2024-01-13 13:29:17 +0800

    feat: add consumer on list view

--- lib/views/movie_list_view.dart
+++ lib/views/movie_list_view.dart
@@ -1,40 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/controller/movie_controller.dart';
 import 'package:movie_app/widgets/movie_container.dart';
+import 'package:provider/provider.dart';
 
 class MovieListView extends StatelessWidget {
   const MovieListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final movieController = MovieController();
-    final movies = movieController.movies;
-    return Scaffold(
-      backgroundColor: const Color(0XFF17141F),
-      appBar: AppBar(
-        backgroundColor: const Color(0XFF17141F),
-        centerTitle: true,
-        title: const Text(
-          "Movies",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: const [
-          Icon(
-            Icons.add,
-            color: Colors.white,
-          )
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: movies.length,
-        itemBuilder: (context, index) {
-          return MovieContainer(movie: movies[index],);
-        },
-      )
+    return Consumer<MovieController>(
+      builder: (context, movieController, child) {
+        final movies = movieController.movies;
+        return Scaffold(
+            backgroundColor: const Color(0XFF17141F),
+            appBar: AppBar(
+              backgroundColor: const Color(0XFF17141F),
+              centerTitle: true,
+              title: const Text(
+                "Movies",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              actions: const [
+                Icon(
+                  Icons.add,
+                  color: Colors.white,
+                )
+              ],
+            ),
+            body: ListView.builder(
+              itemCount: movies.length,
+              itemBuilder: (context, index) {
+                return MovieContainer(movie: movies[index],);
+              },
+            )
+        );
+      },
     );
   }
 }
